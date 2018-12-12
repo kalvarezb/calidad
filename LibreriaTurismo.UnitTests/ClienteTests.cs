43810b65751d36c4fdc040478df85efdc9ef26af
Katherine <kalvarezb90@gmail.com>
2018-12-11 23:27:20 -0300

    mejorando tests

--- LibreriaTurismo.UnitTests/ClienteTests.cs
+++ LibreriaTurismo.UnitTests/ClienteTests.cs
@@ -10,38 +10,45 @@ namespace LibreriaTurismo.UnitTests
         [TestMethod]
         public void ValidarEmail_EmailIsValid_ReturnsTrue()
         {
-            var cliente = new Cliente("173180101", "nombre", "12345678", "email@email.com").validarEmail("email@email.com");
+            var email = new Cliente("173180101", "name", "12345678", "email@email.com").validarEmail("email@email.com");
           
-            Assert.IsTrue(cliente);
+            Assert.IsTrue(email);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void ValidarEmail_EmailIsNotValid_ReturnsArgumentException()
         {
-            var cliente = new Cliente("173180101", "nombre", "12345678", "not valid email").validarEmail("not valid email");
+            var email = new Cliente("173180101", "name", "12345678", "not valid email").validarEmail("not valid email");
+
+            Assert.IsFalse(email);
         }
 
         [TestMethod]
         [ExpectedException(typeof(FormatException))]
         public void ValidarRut_RutIsNotValid_ReturnsFormatException()
         {
-            var cliente = new Cliente("not valid rut", "nombre", "12345678", "valid@email.com");
+            var cliente = new Cliente("not valid rut", "name", "12345678", "valid@email.com");
+
+            Assert.IsNull(cliente);
         }
 
         [TestMethod]
         public void ValidarRut_RutIsValid_ReturnsClient()
         {
-            var cliente = new Cliente("173180101", "nombre", "12345678", "valid@email.com");
+            var cliente = new Cliente("173180101", "name", "12345678", "valid@email.com");
 
+            Assert.IsNotNull(cliente);
             Assert.IsInstanceOfType(cliente, typeof(Cliente));
         }
 
         [TestMethod]
         public void ValidarRut_RutIsValid_ReturnsRutWithCorrectFormat()
         {
-            var cliente = new Cliente("17.318.010-1", "nombre", "12345678", "valid@email.com");
-            
+            var cliente = new Cliente("17.318.010-1", "name", "12345678", "valid@email.com");
+
+            Assert.IsNotNull(cliente);
+            Assert.IsInstanceOfType(cliente, typeof(Cliente));
             Assert.IsTrue(cliente.Rut == "17.318.010-1");
         }
 
@@ -50,6 +57,8 @@ namespace LibreriaTurismo.UnitTests
         public void NombreLength_NombreHasLessThanThreeCharacters_ReturnsArgumentException()
         {
             var cliente = new Cliente("173180101", "pi", "12345678", "valid@email.com");
+
+            Assert.IsNull(cliente);
         }
 
         [TestMethod]
@@ -57,13 +66,17 @@ namespace LibreriaTurismo.UnitTests
         public void NombreFormat_NombreHasNumbers_ReturnsArgumentException()
         {
             var cliente = new Cliente("173180101", "123name123", "12345678", "valid@email.com");
+
+            Assert.IsNull(cliente);
         }
 
         [TestMethod]
         [ExpectedException(typeof(FormatException), "Doesn't validates invalid input types for Telefono field")]
         public void TelefonoFormat_TelefonoHasLetters_ReturnsFormatException()
         {
-            var cliente = new Cliente("173180101", "123name123", "12345678", "valid@email.com");
+            var cliente = new Cliente("173180101", "name", "12345678", "valid@email.com");
+
+            Assert.IsNull(cliente);
         }
     }
 }
